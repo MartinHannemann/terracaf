74404519d2432f7a45ee679474ee1f0e9e51fd3f
martinhannemann <1241294+MartinHannemann@users.noreply.github.com>
2025-04-24 14:40:04 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used.
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.107"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# You can use the azurerm_client_config data resource to dynamically
+# extract connection settings from the provider configuration.
+
+data "azurerm_client_config" "core" {}
+
+# Declare the Azure landing zones Terraform module
+# and provide a base configuration.
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "6.2.1"
+
+  default_location = "westeurope"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = "bilr"
+  root_name      = "Bilr"
+
+  deploy_demo_landing_zones = true
+}
